99fb4e28b7adf284003089828f29cd1d911847a7
AlxMorgen <sanyamatus@gmail.com>
2020-06-01 18:05:53 +0300

    Добавил слайдер

---
+++ app.js
@@ -0,0 +1,16 @@
+/* testimonials */
+
+let slider = $("#testimonialsSlider");
+let appendDots = $("#testiphoto");
+slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: true,
+    dots: true,
+    rows: window
+  });
+
+  
+
